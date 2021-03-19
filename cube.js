b7337ed26e560d8bf298ea9f4cb0ed2a79d90348
ciscou <francismpp@gmail.com>
2021-03-19 22:56:34 +0100

    rotating front slice

--- cube.js
+++ cube.js
@@ -1,17 +1,24 @@
 (function() {
   var cube = document.querySelector(".cube");
+  var frontSlice = cube.querySelectorAll(".cubie-container.front");
 
-  var rx=-10;
+  var rx=-20;
   var ry=-20;
   var rz=0;
+  var f = 0;
 
   function tick() {
     cube.style.transform = "rotateX(" + rx + "deg) rotateY(" + ry + "deg) rotateZ(" + rz + "deg)";
+    frontSlice.forEach(function(cubieContainer) {
+      cubieContainer.style.transform = "rotateZ(" + f + "deg)"
+    });
 
     rx -= 0;
-    ry -= 90;
+    ry -= 30;
     rz += 0;
+    f += 90;
   }
 
   setInterval(tick, 1000);
+  // tick();
 })();
